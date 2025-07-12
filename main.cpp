861ee993dd76fe2ee5574b2a94b765f2f0e4e89f
Agata Rybarska <arybarsk@c3r9p3.42vienna.com>
2025-07-12 15:52:39 +0200

    Take comments out of the main function

--- main.cpp
+++ main.cpp
@@ -24,31 +24,40 @@ void	handle_sig(int sig)
 	g_sigCaught = sig;
 }
 
+// this: struct sigaction sa = {};
+// would be better instead of memset (cleaner, more type-safe)
+// but in C++98, this is technically not standard.
+
+// with SA_RESTART the system automatically checks if errno == EINTR
+// for every syscall like poll() interrupted by a signal
+
+// sigemptyset() is forbidden in the subject
+// otherwise, this line would allow only SIGINT during handler:
+// sigemptyset(sa.sa_mask);
+// here we can live without it because we memset() the whole struct to zero
+// which is effectively the same as sigemptyset()
+
+// SIGINT is Ctrl-C
+// SIGTERM is in kill command:
+// ps aux | grep ircserv
+// kill -SIGTERM <process number>
+
+// SIGPIPE is ignored so send/recv errors can be handled in code, not via signals
+
 int main(int argc, char **argv)
 {	
-	// this would be better instead of memset (cleaner, more type-safe):
-	// struct sigaction sa = {};
-	// but in C++98, this is technically not standard.
 	struct sigaction sa;
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = handle_sig;
-	// with SA_RESTART the system automatically checks if errno == EINTR
-	// for every syscall like poll() interrupted by a signal
 	sa.sa_flags = SA_RESTART;
-	// sigemptyset() is forbidden in the subject
-	// otherwise, this line would allow only SIGINT during handler:
-	// sigemptyset(sa.sa_mask);
-	// here we can live without it because we memset() the whole struct to zero
-	// which is effectively the same as sigemptyset()
 	
-	if (sigaction(SIGINT, &sa, NULL) == -1 // for Ctrl-C
-		|| sigaction(SIGTERM, &sa, NULL) == -1) // for kill command
+	if (sigaction(SIGINT, &sa, NULL) == -1 
+		|| sigaction(SIGTERM, &sa, NULL) == -1)
 	{
 		perror("sigaction");
 		return (1);
 	}
-	
-	// Ignore SIGPIPE so send/recv errors can be handled in code, not via signals
+
 	struct sigaction sa_pipe;
 	memset(&sa_pipe, 0, sizeof(sa_pipe));
 	sa_pipe.sa_handler = SIG_IGN;
